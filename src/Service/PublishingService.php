cf798a495735f7fb9eef8c7ce997a6ee5bf1c889
Peter <peter.eussen@gmail.com>
2017-03-29 14:13:56 +0200

    Added an optional $body parameter so you can build your own base body to allow for more detailed information on publish
    As an answer to https://github.com/martybel/tripolis-api/issues/2

--- src/Service/PublishingService.php
+++ src/Service/PublishingService.php
@@ -10,60 +10,118 @@ namespace MartyBel\Tripolis\Service;
 class PublishingService extends AbstractService
 {
 
-    public function publishTransactionalEmail($contactId,$emailId,$mailJobTags = array())
+    /**
+     * Publishes as tranactional Email
+     *
+     * @param $contactId
+     * @param $emailId
+     * @param array $mailJobTags
+     * @param array|bool $body
+     * @return mixed
+     */
+    public function publishTransactionalEmail($contactId,$emailId,$mailJobTags = array(),$body = false)
     {
-        $body = array(
-            'contactId'     => $contactId,
-            'directEmailId' => $emailId,
-            'mailJobTagIds' => array(
-                'mailJobTagId' => $mailJobTags
-            )
-        );
+        if ( $body ) {
+            $body['contactId']      = $contactId;
+            $body['directEmailId']  = $emailId;
+
+            if ( $mailJobTags ) {
+                if ( isset($body['mailJobTagIds'])) {
+                    $body['mailJobTagIds'] = array_unique(array_merge($body['mailJobTagIds'],$mailJobTags));
+                } else {
+                    $body['mailJobTagIds'] = $mailJobTags;
+                }
+            }
+        } else {
+            $body = array(
+                'contactId'     => $contactId,
+                'directEmailId' => $emailId,
+                'mailJobTagIds' => array(
+                    'mailJobTagId' => $mailJobTags
+                )
+            );
+        }
 
         return $this->invoke(__FUNCTION__,$body);
     }
 
   /**
+   * Send a newsletter based on the emailId and the contact group you want to sent it to.
    *
    * @param $eId
    * @param $contactGroupId
    * @param array $mailJobTags
+   * @param array|bool $body
    * @return mixed
    */
-    public function publishEmail($eId, $contactGroupId, $mailJobTags = array())
+    public function publishEmail($eId, $contactGroupId, $mailJobTags = array(),$body = false)
     {
-      $body = array(
-        'contactGroupId' => $contactGroupId,
-        'newsletterId'	 => $eId,
-        'mailsPerHour'	 => 10000,
-        'mailJobTagIds' => array(
-          'mailJobTagId' => $mailJobTags
-        )
-      );
+        if ( $body ) {
+            $body['contactGroupId'] = $contactGroupId;
+            $body['newsletterId']   = $eId;
+
+            if ( !isset($body['mailsPerHour'])) {
+                $body['mailsPerHour'] = 10000;
+            }
+
+            if ( $mailJobTags ) {
+                if ( isset($body['mailJobTagIds'])) {
+                    $body['mailJobTagIds'] = array_unique(array_merge($body['mailJobTagIds'],$mailJobTags));
+                } else {
+                    $body['mailJobTagIds'] = $mailJobTags;
+                }
+            }
+        } else {
+            $body = array(
+                'contactGroupId' => $contactGroupId,
+                'newsletterId'	 => $eId,
+                'mailsPerHour'	 => 10000,
+                'mailJobTagIds'  => array(
+                    'mailJobTagId' => $mailJobTags
+                )
+            );
+        }
 
       return $this->invoke(__FUNCTION__,$body);
     }
 
   /**
+   * Send a Direct mail to a contact group based on the direct mail id and the body
    *
    * @param $dmId
    * @param $contactGroupId
    * @param array $mailJobTags
+   * @param array|bool $body
    * @return mixed
    */
-    public function publishDirectEmail($dmId, $contactGroupId, $mailJobTags = array())
+    public function publishDirectEmail($dmId, $contactGroupId, $mailJobTags = array(), $body = false)
     {
-      $body = array(
-        'contactGroupId' => $contactGroupId,
-        'directEmailId'	 => $dmId,
-        'mailsPerHour'	 => 10000,
-        'mailJobTagIds'  => array(
-          'mailJobTagId' => $mailJobTags
-        )
-      );
+        if ( $body ) {
+            $body['contactGroupId'] = $contactGroupId;
+            $body['directEmailId']  = $dmId;
 
-      //var_dump($body); exit();
+            if ( !isset($body['mailsPerHour'])) {
+                $body['mailsPerHour'] = 10000;
+            }
 
-      return $this->invoke('publishEmail',$body);
+            if ( $mailJobTags ) {
+                if ( isset($body['mailJobTagIds'])) {
+                    $body['mailJobTagIds'] = array_unique(array_merge($body['mailJobTagIds'],$mailJobTags));
+                } else {
+                    $body['mailJobTagIds'] = $mailJobTags;
+                }
+            }
+        } else {
+            $body = array(
+                'contactGroupId' => $contactGroupId,
+                'directEmailId'  => $dmId,
+                'mailsPerHour'   => 10000,
+                'mailJobTagIds'  => array(
+                    'mailJobTagId' => $mailJobTags
+                )
+            );
+        }
+
+        return $this->invoke('publishEmail',$body);
     }
 }
